cd53a69ce77d153de1166197ce1db2e7407900bf
DeveloperMalay <malay13pandit@gmail.com>
2023-02-24 23:57:37 +0530

    changing the static speed value

--- lib/animations/speedometer_screen.dart
+++ lib/animations/speedometer_screen.dart
@@ -19,57 +19,58 @@ class _SpeedoMeterScreenState extends State<SpeedoMeterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 46, 47, 47),
         body: Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TweenAnimationBuilder(
-              tween: Tween<double>(begin: 0.0, end: _speed),
-              duration: const Duration(seconds: 2),
-              builder: (BuildContext context, double value, Widget? child) {
-                return Center(
-                    child: SfRadialGauge(axes: <RadialAxis>[
-                  RadialAxis(minimum: 0, maximum: 150, ranges: <GaugeRange>[
-                    GaugeRange(
-                        startValue: 0,
-                        endValue: 50,
-                        color: Colors.green,
-                        startWidth: 10,
-                        endWidth: 10),
-                    GaugeRange(
-                        startValue: 50,
-                        endValue: 100,
-                        color: Colors.orange,
-                        startWidth: 10,
-                        endWidth: 10),
-                    GaugeRange(
-                        startValue: 100,
-                        endValue: 150,
-                        color: Colors.red,
-                        startWidth: 10,
-                        endWidth: 10)
-                  ], pointers: <GaugePointer>[
-                    NeedlePointer(value: value)
-                  ], annotations: const <GaugeAnnotation>[
-                    GaugeAnnotation(
-                        widget: Text('90.0',
-                            style: TextStyle(
-                                fontSize: 25, fontWeight: FontWeight.bold)),
-                        angle: 90,
-                        positionFactor: 0.5)
-                  ])
-                ]));
-              }),
-          Slider(
-            value: _speed,
-            min: 0,
-            max: 150,
-            onChanged: (double newValue) {
-              _updateSpeed(newValue);
-            },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TweenAnimationBuilder(
+                  tween: Tween<double>(begin: 0.0, end: _speed),
+                  duration: const Duration(seconds: 2),
+                  builder: (BuildContext context, double value, Widget? child) {
+                    return Center(
+                        child: SfRadialGauge(axes: <RadialAxis>[
+                      RadialAxis(minimum: 0, maximum: 150, ranges: <GaugeRange>[
+                        GaugeRange(
+                            startValue: 0,
+                            endValue: 50,
+                            color: Colors.green,
+                            startWidth: 10,
+                            endWidth: 10),
+                        GaugeRange(
+                            startValue: 50,
+                            endValue: 100,
+                            color: Colors.orange,
+                            startWidth: 10,
+                            endWidth: 10),
+                        GaugeRange(
+                            startValue: 100,
+                            endValue: 150,
+                            color: Colors.red,
+                            startWidth: 10,
+                            endWidth: 10)
+                      ], pointers: <GaugePointer>[
+                        NeedlePointer(value: value)
+                      ], annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                            widget: Text(value.toStringAsFixed(2),
+                                style: const TextStyle(
+                                    fontSize: 25, fontWeight: FontWeight.bold)),
+                            angle: 90,
+                            positionFactor: 0.5)
+                      ])
+                    ]));
+                  }),
+              Slider(
+                value: _speed,
+                min: 0,
+                max: 150,
+                onChanged: (double newValue) {
+                  _updateSpeed(newValue);
+                },
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }
