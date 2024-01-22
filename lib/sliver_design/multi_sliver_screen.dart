b7ffb4da9b9b86456b38a8693f1f5671e7de0e7a
Malay Pandit <94185006+DeveloperMalay@users.noreply.github.com>
2024-01-22 16:34:19 +0530

    adding sliver scrolling effect

--- lib/sliver_design/multi_sliver_screen.dart
+++ lib/sliver_design/multi_sliver_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 class MultiSliverScreen extends StatefulWidget {
   const MultiSliverScreen({super.key});
@@ -12,8 +13,74 @@ class _MultiSliverScreenState extends State<MultiSliverScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sliver'),
+        title: const Text('Sliver'),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          Section(
+            title: 'Category #1',
+            headerColor: Colors.blue,
+            items: List.generate(
+                10,
+                (index) => ListTile(
+                      title: Text('Item #${index + 1}'),
+                    )),
+          ),
+          Section(
+            title: 'Category #2',
+            headerColor: Colors.red,
+            items: List.generate(
+                10,
+                (index) => ListTile(
+                      title: Text('Item #${index + 11}'),
+                    )),
+          ),
+          Section(
+            title: 'Category #3',
+            headerColor: Colors.red,
+            items: List.generate(
+                10,
+                (index) => ListTile(
+                      title: Text('Item #${index + 11}'),
+                    )),
+          ),
+          Section(
+            title: 'Category #4',
+            headerColor: Colors.red,
+            items: List.generate(
+                10,
+                (index) => ListTile(
+                      title: Text('Item #${index + 11}'),
+                    )),
+          ),
+        ],
       ),
     );
   }
+}
+
+class Section extends MultiSliver {
+  Section({
+    Key? key,
+    required String title,
+    Color headerColor = Colors.white,
+    Color titleColor = Colors.black,
+    required List<Widget> items,
+  }) : super(
+          key: key,
+          pushPinnedChildren: true,
+          children: [
+            SliverPinnedHeader(
+                child: ColoredBox(
+              color: headerColor,
+              child: ListTile(
+                textColor: titleColor,
+                title: Text(title),
+              ),
+            )),
+            SliverList(
+              delegate: SliverChildListDelegate.fixed(items),
+            ),
+          ],
+        );
 }
